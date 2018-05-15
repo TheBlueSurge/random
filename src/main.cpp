3bef6b587781b9c9e306d55205cee58e35a12651
TheBlueSurge <TheBlueSurge@users.noreply.github.com>
2018-05-15 18:21:19 -0400

    Finished Normal Distribution

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include "ofMain.h"
 #include "ofApp.h"
+#include <random>
+#include <vector>
 
 //========================================================================
 int main( ){
--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,10 +1,35 @@
 #include "ofApp.h"
+#include <random>
+#include <vector>
+using namespace std;
 
 //--------------------------------------------------------------
 void ofApp::setup(){
+	
+	random_device rd;
+
+	int minX = 0;
+	int minY = 0;
+	int maxX = 1200;
+	int maxY = 970;
+	
+	mt19937 eng1(rd());
+	
+	uniform_int_distribution<int> XCord(minX,maxX);
+	
+	uniform_int_distribution<int> YCord(minY,maxY);
+	
+	for(int i = 0; i < 20; i++){
+		X.push_back(XCord(eng1));
+	}
+	
+	for (int j = 0; j < 20;j++){
+		Y.push_back(YCord(eng1));
+	}
+	
 jewel.load("diamond.png");// image to use (must be stored in "random/bin/data" folder)
-X = {500, 1000, 30, 100, 300, 200, 750, 380 };// set of x-coordinates for jewel
-Y = {630, 400, 900,40, 200, 0, 90, 30}; //set of y-coordinates for jewel
+//X = {500, 1000, 30, 100, 300, 200, 750, 380 };// set of x-coordinates for jewel
+//Y = {630, 400, 900,40, 200, 0, 90, 30}; //set of y-coordinates for jewel
 }
 
 //--------------------------------------------------------------
